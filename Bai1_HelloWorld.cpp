814ef6101684d403b7b41ebe7e4781523869f217
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-04 11:23:56 +0700

    Add project files.

---
+++ Bai1_HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World\n";
+    cout << " Toi ten la :" << "\tVo Minh Thien" << endl;
+    cout << " Toi dang hoc :" << "\tC++" << endl;
+    return 0;
+}
+
